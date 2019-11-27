1df0b5c366e4c732ed2fd28761f2547344ca0934
Lucas <lucasgilbertoheinz16@hotmail.com>
2019-11-27 11:36:17 -0300

    arquivos excluidos

--- tabela-pessoas/menu.php
+++
@@ -1,5 +0,0 @@
-<div class="row">
-    <a href="?pagina=cadastro" class="btn btn-success">Cadastrar nova pessoa</a>
-    <a href="?pagina=listagem" class="btn btn-primary">Listar Pessoas cadastradas</a>
-</div>
-<hr>
