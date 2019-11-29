6e42cd43fe3e37698c6e8d85b4980f68e3bbda57
Lucas <lucasgilbertoheinz16@hotmail.com>
2019-11-29 06:58:43 -0300

    Produtos

---
+++ Produtoss/categorie.php
@@ -0,0 +1,6 @@
+<form action='categorieAdd.php' method='POST' enctype='multipart/form-data'>
+	<input type='text' name='NomeCategoria'/><br/>
+	<input type='text' name='Descricao'/><br/>
+	<input type='file' name='Figura' id='Figura'/><br/>
+	<input type='submit'/>
+</form>
---
+++ Produtoss/categorieAdd.php
@@ -0,0 +1,21 @@
+<?php
+	if(isset($_POST)){
+		try {
+			$dbName = 'lucas';
+			$dbPass = '';
+			$dbUser = 'root';
+			$dbHost = 'localhost';
+			$dbPort = '3306';
+			$con = new PDO('mysql:dbname=' . $dbName . ';host=' . $dbHost . ':' . $dbPort, $dbUser, $dbPass);
+		} catch(Throwable $e) {
+			die('<p>' . $e->getMessage() . '</p>');
+		}
+		$figura = 'data:'.$_FILES['Figura']['type'].';base64,'.base64_encode(file_get_contents($_FILES['Figura']['tmp_name']));
+		$dataStmt = $con->prepare('INSERT INTO categorias (NomeCategoria, Descricao, Figura) VALUES (?,?,?)');
+		
+		$dataStmt->execute([$_POST['NomeCategoria'], 
+			$_POST['Descricao'], 
+			$figura]);
+	}
+	header('Location: categorie.php');
+?>
